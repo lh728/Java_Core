aed7d923e145712f90965d788675339aae4c1d98
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-02-02 21:51:54 +0800

    Create Manager.java

---
+++ com/Manager.java
@@ -0,0 +1,61 @@
+package com;
+
+public class Manager extends Employee{
+    private double bonus ;
+
+    public Manager() {
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    /**
+     * @param name the employee's name
+     * @param salary the employee's salary
+     * @param year the employee's hire year
+     * @param month the employee's hire month
+     * @param day the employee's hire day
+     */
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    @Override
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Manager manager = (Manager) o;
+
+        return Double.compare(manager.bonus, bonus) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(bonus);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Manager{" +
+                "bonus=" + bonus +
+                '}';
+    }
+}
