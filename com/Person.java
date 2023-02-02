9944daf41f3c0d7e753e745924b1b02f8223bc83
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-02-02 21:51:17 +0800

    Create Person.java

---
+++ com/Person.java
@@ -0,0 +1,22 @@
+package com;
+
+public abstract class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract String getDescription();
+}
