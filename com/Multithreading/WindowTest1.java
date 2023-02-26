61318763d87817d0addb580dc96e0fa808209114
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-02-26 21:59:36 +0800

    Create WindowTest1.java

---
+++ com/Multithreading/WindowTest1.java
@@ -0,0 +1,34 @@
+package com_second.Multithreading;
+
+public class WindowTest1{
+    public static void main(String[] args) {
+        Window1 window1 = new Window1();
+        Thread thread = new Thread(window1);
+        Thread thread2 = new Thread(window1);
+        Thread thread3 = new Thread(window1);
+
+        thread.setName("window1");
+        thread2.setName("window2");
+        thread3.setName("window3");
+
+        thread.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+class Window1 implements Runnable{
+    private int ticket = 100;
+
+    @Override
+    public void run() {
+        while (true ){
+            if (ticket > 0){
+
+                System.out.println(Thread.currentThread().getName() + ":Now the ticket is " + ticket );
+                ticket--;
+            }else {
+                break;
+            }
+        }
+    }
+}
