38ea390b74615e83105b6599d63dc21f090fe1f8
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-02-27 21:31:35 +0800

    Update WindowTest1.java

--- com/Multithreading/WindowTest1.java
+++ com/Multithreading/WindowTest1.java
@@ -1,5 +1,9 @@
 package com_second.Multithreading;
 
+/**
+ * @author lhj
+ * @create 2023-2-26
+ */
 public class WindowTest1{
     public static void main(String[] args) {
         Window1 window1 = new Window1();
@@ -18,16 +22,19 @@ public class WindowTest1{
 }
 class Window1 implements Runnable{
     private int ticket = 100;
+    Object obj = new Object();
 
     @Override
     public void run() {
-        while (true ){
-            if (ticket > 0){
 
-                System.out.println(Thread.currentThread().getName() + ":Now the ticket is " + ticket );
-                ticket--;
-            }else {
-                break;
+        while (true ){
+            synchronized (obj) {
+                if (ticket > 0) {
+                    System.out.println(Thread.currentThread().getName() + ":Now the ticket is " + ticket);
+                    ticket--;
+                } else {
+                    break;
+                }
             }
         }
     }
