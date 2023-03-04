5f05d403ca5d009c57fe9da79514aabd7eb68fc1
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-03-04 13:52:07 +0800

    Create ThreadTest.java

---
+++ com/Multithreading/ThreadTest.java
@@ -0,0 +1,62 @@
+package com_second.Multithreading;
+
+/**
+ * DeadLock
+ */
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+
+        new Thread(){
+            @Override
+            public void run() {
+                synchronized (s1){
+                    s1.append("a");
+                    s2.append("1");
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    synchronized (s2){
+                        s1.append("b");
+                        s2.append("2");
+
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+            }
+        }.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (s2){
+                    s1.append("c");
+                    s2.append("3");
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    synchronized (s1){
+                        s1.append("d");
+                        s2.append("4");
+
+                        System.out.println(s1);
+                        System.out.println(s2);
+                    }
+                }
+            }
+        }).start();
+
+    }
+}
