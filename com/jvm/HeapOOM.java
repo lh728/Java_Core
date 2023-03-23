953624491f3804721fc6bb0fc30517b8e97e5993
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-03-23 22:27:16 +0800

    Create HeapOOM.java

---
+++ com/jvm/HeapOOM.java
@@ -0,0 +1,21 @@
+package jvm;
+
+import java.util.ArrayList;
+
+
+/*
+-Xms20m
+-Xmx20m
+-XX:+HeapDumpOnOutOf-MemoryError
+ */
+
+public class HeapOOM {
+    static class OOMObject{}
+
+    public static void main(String[] args) {
+        ArrayList<OOMObject> list = new ArrayList<>();
+        while (true){
+            list.add(new OOMObject());
+        }
+    }
+}
