d6cb935d6c6bc260f081201f6a144a31fab324a9
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-04-09 14:54:09 +0800

    Update DirectMemoryOOM.java

--- com/jvm/DirectMemoryOOM.java
+++ com/jvm/DirectMemoryOOM.java
@@ -5,7 +5,7 @@ import sun.misc.Unsafe;
 import java.lang.reflect.Field;
 
 public class DirectMemoryOOM {
-    private static final int _1MB = 1024 * 1024;
+    private static final int _1MB = 1024 * 1024; 
  
     public static void main(String[] args) throws IllegalAccessException {
         Field field = Unsafe.class.getDeclaredFields()[0];
