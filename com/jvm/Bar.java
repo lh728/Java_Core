34110c2df063b2cc91c20853730eb184697588e1
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-04-18 21:32:34 +0800

    Create Bar.java

---
+++ com/jvm/Bar.java
@@ -0,0 +1,18 @@
+package net.javaguides.spring.ioc;
+
+/*
+java -XX:+PrintAssembly -Xcomp -XX:CompileCommand=dontinline,*Bar.sum
+-XX:CompileCommand=compileonly,*Bar.sum test.Bar
+ */
+public class Bar {
+    int a = 1;
+    static int b = 1;
+
+    public static void main(String[] args) {
+        new Bar().sum(3);
+    }
+
+    private int sum(int c) {
+        return a + b + c;
+    }
+}
