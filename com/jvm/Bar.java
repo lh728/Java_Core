874db426043564d44b28f22b00fdac6c585dcfee
val (Hongjin Lu) <65521533+lh728@users.noreply.github.com>
2023-04-19 21:25:49 +0800

    Update Bar.java

--- com/jvm/Bar.java
+++ com/jvm/Bar.java
@@ -5,7 +5,7 @@ java -XX:+PrintAssembly -Xcomp -XX:CompileCommand=dontinline,*Bar.sum
 -XX:CompileCommand=compileonly,*Bar.sum test.Bar
  */
 public class Bar {
-    int a = 1;
+    int a = 1; 
     static int b = 1;
 
     public static void main(String[] args) {
